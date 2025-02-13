2787947c1d8e86e28cba841f5b0ef2342ba7d59f
Rico De Santis <ricodesantis@gmail.com>
2025-02-12 22:16:04 -0500

    build desktop header and start working through mobile

---
+++ data/header/navigation.tsx
@@ -0,0 +1,85 @@
+const navigation = {
+        'logo':{
+            'url':'/images/coastal-clear-logo.png',
+            'altText':''
+        },
+        primaryNav:[
+            {
+                'settings':{
+                    'hasSubMenu': false,
+                },
+                'topLevelLink':{
+                    'text': 'About',
+                    'link': "/about",
+                    'targetBlank': false,
+                    'addCtaStyle': false
+                },
+            },
+            {
+                'settings':{
+                    'hasSubMenu': true,
+                },
+                'topLevelLink':{
+                    'text': 'Services',
+                    'link': "/services",
+                    'targetBlank': false,
+                    'addCtaStyle': false
+                },
+                'subNav':{
+                    'header': 'Services',
+                    'dropdownItems': [
+                        {
+                            'text': 'All Services',
+                            'link': "/services",
+                            'targetBlank': false,
+                        },
+                        {
+                            'text': 'Pool Heaters',
+                            'link': "/services/pool-heater",
+                            'targetBlank': false,
+                        },
+                        {
+                            'text': 'Power Washing',
+                            'link': "/services/power-washing",
+                            'targetBlank': false,
+                        },
+                        {
+                            'text': 'Pool Resurfacing',
+                            'link': "/services/pool-resurfacing",
+                            'targetBlank': false,
+                        },
+                        {
+                            'text': 'Pool Renovations',
+                            'link': "/services/pool-renovations",
+                            'targetBlank': false,
+                        },
+                    ]
+                }
+            },
+            {
+                'settings':{
+                    'hasSubMenu': false,
+                },
+                'topLevelLink':{
+                    'text': 'FAQs',
+                    'link': "/company/faqs",
+                    'targetBlank': false,
+                    'addCtaStyle': false
+                },
+            },
+            {
+                'settings':{
+                    'hasSubMenu': false,
+                },
+                'topLevelLink':{
+                    'text': 'Contact Us',
+                    'link': "tel:123-456-7890",
+                    'targetBlank': true,
+                    'addCtaStyle': true
+                },
+            },
+
+        ]
+    };
+
+export default navigation
