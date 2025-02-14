a30b565f089a3eb9468d3693c57cdf19af194e51
Rico De Santis <ricodesantis@gmail.com>
2025-02-13 22:38:32 -0500

    add footer and clean up global style

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+import Style from './Footer.module.scss';
+import {Footer as FooterType } from '../../typescript/component'
+
+export const Footer = ({settings}:FooterType) => {
+  return (
+    <footer className={`${Style.footer}`}>
+      <div className={`container`}>
+        <div className={'row align-itmes-center gap-lg remove-margin'}>
+            <div className={`col ${Style.leftColumm}`}>
+                <p className={`col ${Style.copyright}`}>Coastal Clear Pools &copy; {new Date().getFullYear()}</p>
+            </div>
+            <div className={`col ${Style.rightColumm}`}>
+                <address className={`${Style.address}`}>Palm Beach County</address>
+            </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
