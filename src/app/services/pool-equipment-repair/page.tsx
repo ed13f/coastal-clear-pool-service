4452655102ff7cc199bbfb1c1b42f29c57d872c3
Rico De Santis <ricodesantis@gmail.com>
2025-02-26 20:25:59 -0500

    create new page shells

---
+++ src/app/services/pool-equipment-repair/page.tsx
@@ -0,0 +1,43 @@
+import { heroData, serviceTabsData, fullWidthSectionData, logoCarouselSectionData, twoColumnImageSectionData } from '../../../../data/pages/services/pool-equipment-repair';
+
+import { HeroSection } from '../../../../components/HeroSection';
+import { ServiceTabsSection } from '../../../../components/ServiceTabsSection';
+import { FullWidthSection } from '../../../../components/FullWidthSection';
+import { LogoCarouselSection } from '../../../../components/LogoCarouselSection';
+import { TwoColumnImageSection } from '../../../../components/TwoColumnImageSection';
+
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection hero={heroData} />
+      <ServiceTabsSection 
+        settings={serviceTabsData.settings}
+        header={serviceTabsData.header}
+        description={serviceTabsData.description}
+        cards={serviceTabsData.cards}
+      />
+      <FullWidthSection
+        settings={fullWidthSectionData.settings}
+        header={fullWidthSectionData.header}
+        description={fullWidthSectionData.description}
+        cta={fullWidthSectionData.cta}
+        image={fullWidthSectionData.image}
+      />
+      <LogoCarouselSection 
+        settings={logoCarouselSectionData.settings}
+        header={logoCarouselSectionData.header}
+        description={logoCarouselSectionData.description}
+        logos={logoCarouselSectionData.logos}
+        cta={logoCarouselSectionData.cta}
+      />
+      <TwoColumnImageSection
+        settings={twoColumnImageSectionData.settings}
+        header={twoColumnImageSectionData.header}
+        description={twoColumnImageSectionData.description}
+        images={twoColumnImageSectionData.images}
+        cta={twoColumnImageSectionData.cta}
+      />
+    </>
+  );
+}
