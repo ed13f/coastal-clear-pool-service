7648eda7154c4847e713bd329d79cbd07c19907e
Rico De Santis <ricodesantis@gmail.com>
2025-03-02 20:22:08 -0500

    Add Type and data to footer

---
+++ data/footer/index.tsx
@@ -0,0 +1,9 @@
+import { FooterType } from '../../components/Footer/Footer';
+
+const footer:FooterType = {
+    copyRight:"Coastal Clear Pools",
+    tagLine:"Proudly Serving Palm Beach, Broward, and Dade County since 2020"
+
+};
+
+export default footer
